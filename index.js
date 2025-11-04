9cfefb6d3a1df341050db6f0862ecd6918185720
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-04 18:06:57 -0500

    update a5 Asynchronous Communication with HTTP Servers

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
-import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-
+import cors from "cors";
 const app = express();
+app.use(cors());
 
 Lab5(app);
-Hello(app);
-
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
