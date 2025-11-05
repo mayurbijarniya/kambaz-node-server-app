e4426d72fc36ee4647efcd962a340d32d95e5706
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-04 19:53:35 -0500

    update a5 Passing JSON Data to a Server in an HTTP Body

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -1,4 +1,4 @@
-const todos = [
+let todos = [
     { id: 1, title: "Task 1", completed: false },
     { id: 2, title: "Task 2", completed: true },
     { id: 3, title: "Task 3", completed: false },
@@ -32,11 +32,44 @@ const todos = [
         res.json(todos);
       };
 
+      const postNewTodo = (req, res) => {
+        const newTodo = { ...req.body, id: new Date().getTime() };
+        todos.push(newTodo);
+        res.json(newTodo);  // Returns only new todo (not entire array!)
+      };
+
       const removeTodo = (req, res) => {
         const { id } = req.params;
         const todoIndex = todos.findIndex((t) => t.id === parseInt(id)); 
         todos.splice(todoIndex, 1);
         res.json(todos);
+      };
+
+      const deleteTodo = (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+          res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+          return;
+        }
+        todos.splice(todoIndex, 1);
+        res.sendStatus(200);
+      };
+
+      const updateTodo = (req, res) => {
+        const { id } = req.params;
+        const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+        if (todoIndex === -1) {
+          res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+          return;
+        }
+        todos = todos.map((t) => {
+          if (t.id === parseInt(id)) {
+            return { ...t, ...req.body };  // Merge original + updates
+          }
+          return t;
+        });
+        res.sendStatus(200);
       };
 
       const updateTodoTitle = (req, res) => {
@@ -60,10 +93,13 @@ const todos = [
         res.json(todos);
       };
     app.get("/lab5/todos/:id/delete", removeTodo);
+    app.delete("/lab5/todos/:id", deleteTodo);
+    app.put("/lab5/todos/:id", updateTodo);
     app.get("/lab5/todos/:id/title/:title", updateTodoTitle);
     app.get("/lab5/todos/:id/completed/:completed", updateTodoCompleted);
     app.get("/lab5/todos/:id/description/:description", updateTodoDescription);
     app.get("/lab5/todos/create", createNewTodo);
+    app.post("/lab5/todos", postNewTodo);
     app.get("/lab5/todos/:id", getTodoById);
     app.get("/lab5/todos", getTodos);
   }
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 Lab5(app);
 app.listen(4000);
