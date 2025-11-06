26cce79e0ff6a5e6e2d14e93772236532f7ee84c
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-06 16:04:47 -0500

    update a5 Integrating the React Profile Screen with a RESTful Web API

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import db from "./Kambaz/Database/index.js";
+import UserRoutes from "./Kambaz/Users/routes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app, db);
 Lab5(app);
 app.listen(4000);
