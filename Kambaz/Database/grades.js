5d79bbe7546c54f012de0ab63512d3ce0cb8a8f0
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-06 14:42:15 -0500

    update a5 Migrating the Database to the Server

---
+++ Kambaz/Database/grades.js
@@ -0,0 +1,12 @@
+export default [
+    { "_id": "1", "user": "123", "course": "RS101", "grade": "A" },
+    { "_id": "2", "user": "234", "course": "RS101", "grade": "B" },
+    { "_id": "3", "user": "345", "course": "RS101", "grade": "C" },
+    { "_id": "4", "user": "456", "course": "RS101", "grade": "D" },
+    { "_id": "5", "user": "567", "course": "RS101", "grade": "E" },
+    { "_id": "6", "user": "234", "course": "RS102", "grade": "A" },
+    { "_id": "7", "user": "789", "course": "RS102", "grade": "B" },
+    { "_id": "8", "user": "890", "course": "RS102", "grade": "C" },
+    { "_id": "9", "user": "123", "course": "RS102", "grade": "D" },
+    { "_id": "10", "user": "234", "course": "RS103", "grade": "E" },
+];
