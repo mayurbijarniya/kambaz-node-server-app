3488cedf7371ddf0e1b27117e408120188cad099
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-06 20:09:38 -0500

    update a5 Supporting Multiple User Sessions

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+import "dotenv/config";
 import express from "express";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import session from "express-session";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
+
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
+}
+
+app.use(session(sessionOptions));
 app.use(express.json());
+
 UserRoutes(app, db);
 Lab5(app);
-app.listen(4000);
+
+const port = process.env.PORT || 4000;
+app.listen(port);
