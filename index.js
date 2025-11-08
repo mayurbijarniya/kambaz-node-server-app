f300afb69819c1b5ffd0ef4e68fdfa02635b8efe
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-07 23:56:47 -0500

    Final A5

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   };
 }
 
