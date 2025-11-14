a3df19356aa8a8b5a34a3722b26270bdc612a229
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-14 10:50:21 -0500

    storing courses in db

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -1,33 +1,27 @@
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
+
 export default function CoursesDao(db) {
   function findAllCourses() {
-    return db.courses;
+    return model.find();
   }
 
   function createCourse(course) {
     const newCourse = { ...course, _id: uuidv4() };
-    db.courses = [...db.courses, newCourse];
-    return newCourse;
+    return model.create(newCourse);
   }
 
   function deleteCourse(courseId) {
-    const { courses, enrollments } = db;
-    db.courses = courses.filter((course) => course._id !== courseId);
-    db.enrollments = enrollments.filter(
-      (enrollment) => enrollment.course !== courseId
-    );
-    return { status: "ok" };
+    return model.deleteOne({ _id: courseId });
   }
 
   function updateCourse(courseId, courseUpdates) {
-    const { courses } = db;
-    const course = courses.find((course) => course._id === courseId);
-    Object.assign(course, courseUpdates);
-    return course;
+    return model.updateOne({ _id: courseId }, { $set: courseUpdates });
   }
 
-  function findCoursesForEnrolledUser(userId) {
-    const { courses, enrollments } = db;
+  async function findCoursesForEnrolledUser(userId) {
+    const courses = await model.find();
+    const { enrollments } = db;
     const enrolledCourses = courses.filter((course) =>
       enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id)
     );
